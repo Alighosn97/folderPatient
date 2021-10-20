98af416f54b7a1020812178ee9a96aa292309594
Ali GHOSN <Alighosn07@gmail.com>
2021-10-20 11:56:11 +0100

    add comment

--- src/main/java/ma/emsi/springmvc/web/PatientController.java
+++ src/main/java/ma/emsi/springmvc/web/PatientController.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 @Controller
 public class PatientController {
+    //commentaire
     @Autowired
     PatientRepository patientRepository;
     @GetMapping(path = "/index")
